836980ef67f94cc911de46ebf1032b58e3a7e53b
Vignesh <154013328+vickynot-10@users.noreply.github.com>
2024-07-26 13:15:07 +0530

    commit on token expire

--- utils/jwttokengenerate.js
+++ utils/jwttokengenerate.js
@@ -3,7 +3,7 @@ import { configDotenv } from "dotenv";
 configDotenv()
 export const generateToken = (user) => {
     try {
-        return jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: "10m" });
+        return jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: "5m" });
     } catch (e) {
         console.log(e)
     }
